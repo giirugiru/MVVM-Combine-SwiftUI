b64b52c35f39e5e671d858ece1d33fddc2d0321e
Gilang <giirugiru@gmail.com>
2024-06-24 10:02:40 +0700

    Initial Commit

---
+++ MVVM-Combine-SwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MVVM-Combine-SwiftUI
+//
+//  Created by Gilang Sinawang on 24/06/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MVVM-Combine-SwiftUI/MVVM_Combine_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVM_Combine_SwiftUIApp.swift
+//  MVVM-Combine-SwiftUI
+//
+//  Created by Gilang Sinawang on 24/06/24.
+//
+
+import SwiftUI
+
+@main
+struct MVVM_Combine_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
